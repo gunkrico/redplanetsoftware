d9870695445a3f0240e5e70f1aa92c1b0cf4c72c
Riko M <riko.mahendra@thekendoway.com>
2024-02-15 14:13:20 +0800

    002 #Remove unused code

--- UserManagement-UI/src/app/users/list.component.ts
+++ UserManagement-UI/src/app/users/list.component.ts
@@ -1,12 +1,10 @@
 
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
-
 import { AccountService } from '@app/_services';
 
 @Component({ templateUrl: 'list.component.html' })
 export class ListComponent implements OnInit {
-    // users?: any[];
     items: any[] = [];
     pageOfItems?: Array<any>;
     sortProperty: string = 'id';
